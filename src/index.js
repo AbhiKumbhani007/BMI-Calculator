8ca300706f9b02a021d05a601dafbd6dd4c39726
AbhiKumbhani007 <abhi1234kumbhani@gmail.com>
2021-07-14 13:18:44 +0530

    1st commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Bmi from './Bmi'
+
+ReactDOM.render(
+  <React.StrictMode>
+  <Bmi />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
